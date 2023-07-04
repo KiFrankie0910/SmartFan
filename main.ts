2dd2d6e184ee5d039db7091fee9ec037970abe95
Ki Frankie <f14091042@pmcps.edu.hk>
2023-07-04 16:10:48 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,11 +7,11 @@ basic.forever(function () {
     OLED.writeNum(temp)
     if (temp > 30) {
         music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
-        pins.digitalWritePin(DigitalPin.P3, 1)
+        pins.digitalWritePin(DigitalPin.P2, 1)
         basic.pause(5000)
-        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
         basic.pause(500)
     } else {
-        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
     }
 })
